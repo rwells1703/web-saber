d52d17b4126846127a17b88e2a58735cdddbcdd3
Richard Wells <rwells1703@gmail.com>
2025-10-19 14:26:39 +0100

    Initial commit - project created prior to using Git.

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var fs = require('fs')
+var https = require('https');
+var socketio = require('socket.io');
+
+
+var app = express();
+
+app.get('/display', (req, res) => {
+    res.sendFile(__dirname + '/webroot/display.html');
+});
+
+app.get('/display_debug', (req, res) => {
+    res.sendFile(__dirname + '/webroot/display_debug.html');
+});
+
+app.get('/phone', (req, res) => {
+    res.sendFile(__dirname + '/webroot/phone.html');
+})
+
+app.use('/libraries', express.static('libraries'));
+app.use('/textures', express.static('textures'));
+app.use('/sounds', express.static('sounds'));
+
+var server = https.createServer({
+    key: fs.readFileSync('./certificates//server.key'),
+    cert: fs.readFileSync('./certificates//server.cert')
+}, app);
+
+
+var io = socketio.listen(server);
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+
+    socket.on('orientation', (orientation) => {
+        io.emit('orientation', orientation);
+    });
+
+    socket.on('calibrate', () => {
+        io.emit('calibrate');
+    })
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+
+server.listen(8080, () => {
+  console.log('listening on *:8080');
+});
